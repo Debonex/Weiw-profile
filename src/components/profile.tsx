bcf0572bdfcc54eeda3b2c4390736831232c3342
Debonet <719893090@qq.com>
2021-09-06 02:33:06 +0800

    :construction: @Debonex use base64 string instead of url

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -2,7 +2,7 @@
  * @Author: Debonex
  * @Date: 2021-09-03 13:01:51
  * @Last Modified by: Debonex
- * @Last Modified time: 2021-09-04 01:58:16
+ * @Last Modified time: 2021-09-06 02:25:03
  */
 
 import { Component } from 'react'
@@ -29,9 +29,9 @@ class Profile extends Component<ProfileProps> {
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width={this.props.height}
-        height={this.props.width}
-        viewBox={`0 0 ${this.props.height} ${this.props.width}`}
+        width={this.props.width}
+        height={this.props.height}
+        viewBox={`0 0 ${this.props.width} ${this.props.height}`}
         style={styles.global}>
         {!!this.props.baseInfoShow && this.props.baseInfoShow !== 'false' && (
           <BaseInfo {...this.props.baseInfo} />
