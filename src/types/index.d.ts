be52d02a7d62fcdd892396f14027d5555898ee3f
Debonet <719893090@qq.com>
2021-09-16 17:37:58 +0800

    :art: @Deboenx remove useless declare

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,7 +2,7 @@
  * @Author: Debonex
  * @Date: 2021-09-03 13:03:21
  * @Last Modified by: Debonex
- * @Last Modified time: 2021-09-10 18:25:53
+ * @Last Modified time: 2021-09-16 17:08:08
  */
 
 import { CSSProperties } from 'react'
@@ -11,13 +11,6 @@ declare module 'react' {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
     xmlns?: string
   }
-}
-
-declare module '*.svg' {
-  import React from 'react'
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
-  const src: string
-  export default src
 }
 
 export type Theme = {
